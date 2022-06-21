9f5cc3f7b81245300c92fda872f3d102caae66ff
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-06-21 10:29:59 +0100

    created a notes route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,13 @@
 // import express using a router class
 const { Router } = require("express");
 
-// import controller functions from api file
-const { getNotes, deleteNotes, createNotes } = require("../controllers/api");
+//import notes
+const notes = require("./notes");
 
 // create a router instance
 const router = Router();
 
 //routers with middleware functions
-router.get("/", getNotes);
-router.delete("/:id", deleteNotes);
-router.post("/", createNotes);
-
+router.use("./notes", notes);
 // export file
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,19 +5,6 @@ const path = require("path");
 // internal imports
 const routes = require("./routes");
 
-// declare logger middleware function to track requests
-// const logger = (req, res, next) => {
-//   // get the request method
-//   const method = req.method;
-//   const path = req.path;
-
-//   // log method and path of request
-//   console.log(`[INFO]: ${method} request for ${path}`);
-
-//   // call next fn to execute next middleware
-//   next();
-// };
-
 // create express instance for app
 const app = express();
 
@@ -25,7 +12,6 @@ const app = express();
 const PORT = 4000;
 
 // middleware functions
-// app.use(logger());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../dist")));
